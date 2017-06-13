f13cbe7f7661ffa72b972fdf721c8647b950776b
ChenYuelun <911172573@qq.com>
2017-06-13 13:11:23 +0800

    首页页面布局

--- app/src/main/java/com/example/shoppingmall2/home/HomeFragment.java
+++ app/src/main/java/com/example/shoppingmall2/home/HomeFragment.java
@@ -1,12 +1,21 @@
 package com.example.shoppingmall2.home;
 
 import android.graphics.Color;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.shoppingmall2.R;
 import com.example.shoppingmall2.base.BaseFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 /**
  * Created by chenyuelun on 2017/6/13.
@@ -14,7 +23,15 @@ import com.example.shoppingmall2.base.BaseFragment;
 
 public class HomeFragment extends BaseFragment {
     private static final String TAG = HomeFragment.class.getSimpleName();//"HomeFragment"
-    private TextView textView;
+    @BindView(R.id.tv_search_home)
+    TextView tvSearchHome;
+    @BindView(R.id.tv_message_home)
+    TextView tvMessageHome;
+    @BindView(R.id.rv_home)
+    RecyclerView rvHome;
+    @BindView(R.id.ib_top)
+    ImageButton ibTop;
+    Unbinder unbinder;
 
     /**
      * 初始化控件
@@ -22,18 +39,38 @@ public class HomeFragment extends BaseFragment {
      */
     @Override
     public View initView() {
-        Log.e(TAG,"初始化主页控件...");
-        textView = new TextView(mContext);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextSize(25);
-        textView.setTextColor(Color.RED);
-        return textView;
+        Log.e(TAG, "初始化主页控件");
+        View view = View.inflate(mContext, R.layout.fragment_home, null);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
     }
 
     @Override
     public void initData() {
         super.initData();
-        Log.e(TAG,"绑定数据到控件上...");
-        textView.setText("HomeFragment");
+        Log.e(TAG, "绑定数据到控件上");
+    }
+
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @OnClick({R.id.tv_search_home, R.id.tv_message_home, R.id.ib_top})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.tv_search_home:
+                Toast.makeText(mContext, "搜索", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tv_message_home:
+                Toast.makeText(mContext, "查看", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ib_top:
+                Toast.makeText(mContext, "回到顶部", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
